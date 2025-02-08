91edf5a28fbae564131bf0d6a291bd55a1063196
Lafiasabigado <lafiasabigado97@gmail.com>
2025-02-08 11:20:57 +0100

    add swagger url

--- footplayers/urls.py
+++ footplayers/urls.py
@@ -7,12 +7,28 @@ from rest_framework.routers import DefaultRouter
 from tutorial.urls import router
 from footplayers import settings
 from fplayers.views import PlayerViewset
+from django.urls import path,re_path
+from drf_yasg import openapi
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
 
 router = DefaultRouter()
 router.register(r'player',PlayerViewset,basename="player")
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Football Players API",
+        default_version="v1",
+        description="Une API pour gérer des joueurs de football",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("admin/", admin.site.urls),
     path("api-auth",include('rest_framework.urls')),
     path('',include(router.urls))
