8263e5da7e83d7bfa2e39daf015912c7ac4ccd3c
Lafiasabigado <lafiasabigado97@gmail.com>
2025-02-10 12:16:23 +0100

    swagger

--- footplayers/settings.py
+++ footplayers/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "fplayers",
     'drf_yasg',
+    "whitenoise.runserver_nostatic"
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
 ROOT_URLCONF = "footplayers.urls"
@@ -160,6 +162,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
