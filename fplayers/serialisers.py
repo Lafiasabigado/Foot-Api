0b20a2f36f77748c81861135781a277e7c5e59b7
Lafiasabigado <lafiasabigado97@gmail.com>
2025-03-07 11:17:46 +0100

    Edit serializers file

--- fplayers/serialisers.py
+++ fplayers/serialisers.py
@@ -4,6 +4,9 @@ from fplayers.models import Player
 
 
 class PlayerSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=255)
+    salary = serializers.IntegerField(min_value=0)
+
     class Meta:
         model = Player
         fields = '__all__'
