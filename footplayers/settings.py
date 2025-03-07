976d70b3cfaacc6bcd4466dd5b752b7cdde83c26
Lafiasabigado <lafiasabigado97@gmail.com>
2025-03-07 17:18:17 +0100

    djoser jwt

--- footplayers/settings.py
+++ footplayers/settings.py
@@ -53,11 +53,13 @@ INSTALLED_APPS = [
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
     "AUTH_HEADER_TYPES": ("Bearer",),
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
 }
 
 DJOSER = {
