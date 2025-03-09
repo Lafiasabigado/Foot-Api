66ea53e3256fa523f181b26e3e5c70c5306563db
Lafiasabigado <lafiasabigado97@gmail.com>
2025-03-09 15:35:33 +0100

    fix bug

--- footplayers/settings.py
+++ footplayers/settings.py
@@ -53,10 +53,10 @@ INSTALLED_APPS = [
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
     "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": False,
+    "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
     "ALGORITHM": "HS256",
     "SIGNING_KEY": SECRET_KEY,
