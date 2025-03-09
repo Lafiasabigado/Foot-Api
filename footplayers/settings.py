1f0d9dc3735b9bfce48888e9aedb9742d3330a9d
Lafiasabigado <lafiasabigado97@gmail.com>
2025-03-09 15:40:40 +0100

    fix bug

--- footplayers/settings.py
+++ footplayers/settings.py
@@ -56,7 +56,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
+    "BLACKLIST_AFTER_ROTATION": False,
     "AUTH_HEADER_TYPES": ("Bearer",),
     "ALGORITHM": "HS256",
     "SIGNING_KEY": SECRET_KEY,
